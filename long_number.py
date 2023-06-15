2e357caae1b04aa0168093ae79c03279a639a33c
Levan Gabiev <lgabiev9@gmail.com>
2023-06-15 12:54:02 +0300

    add long_number.py file

---
+++ long_number.py
@@ -0,0 +1,21 @@
+def numeral_count(n):
+  count=0
+  while n>0:
+    n//=10
+    count+=1
+  return count
+
+def numeral_check(numeral):
+  max_count=0
+  max_number=0
+  for i in range(1,numeral+1):
+    new_value=int(input('введите число'))
+    if new_value<0:
+      new_value=0
+    if numeral_count(new_value)>max_count:
+      max_count=numeral_count(new_value)
+      max_number=new_value
+  return max_number
+
+numeral=int(input('введите количество задач: '))
+print('первая задача на обработку: ', numeral_check(numeral))
